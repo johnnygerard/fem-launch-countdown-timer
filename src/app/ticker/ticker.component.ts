d448b246d83cb29731faf690384e9ddb443571f8
Johnny Gérard <john@jgerard.dev>
2023-06-29 23:14:53 +0200

    feat: generate ticker component

---
+++ src/app/ticker/ticker.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-ticker',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './ticker.component.html',
+  styleUrls: ['./ticker.component.scss']
+})
+export class TickerComponent {
+
+}
