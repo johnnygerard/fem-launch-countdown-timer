ae225ab1abde1bc4e98a41a5aa403c22985d5831
Johnny Gérard <john@jgerard.dev>
2023-07-02 15:20:39 +0200

    feat: enhance timer initialization

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -1,6 +1,14 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TickerComponent } from '../ticker/ticker.component';
+
+const enum TimeSpan {
+  SECOND = 1000,
+  MINUTE = 60 * SECOND,
+  HOUR = 60 * MINUTE,
+  DAY = 24 * HOUR,
+  FOURTEEN_DAYS = 14 * DAY
+}
 
 @Component({
   selector: 'app-timer',
@@ -9,16 +17,30 @@ import { TickerComponent } from '../ticker/ticker.component';
   templateUrl: './timer.component.html',
   styleUrls: ['./timer.component.scss']
 })
-export class TimerComponent implements OnInit {
-  protected days = 14;
-  protected hours = 0;
-  protected minutes = 0;
-  protected seconds = 0;
+export class TimerComponent implements OnDestroy {
+  protected days: number;
+  protected hours: number;
+  protected minutes: number;
+  protected seconds: number;
 
-  private timerId = 0;
+  private timerId: number;
 
-  ngOnInit(): void {
-    this.timerId = window.setInterval(() => this.tick(), 1000);
+  constructor() {
+    let remainingTime = TimeSpan.FOURTEEN_DAYS - (Date.now() % TimeSpan.FOURTEEN_DAYS);
+
+    this.days = Math.floor(remainingTime / TimeSpan.DAY);
+    remainingTime %= TimeSpan.DAY;
+    this.hours = Math.floor(remainingTime / TimeSpan.HOUR);
+    remainingTime %= TimeSpan.HOUR;
+    this.minutes = Math.floor(remainingTime / TimeSpan.MINUTE);
+    remainingTime %= TimeSpan.MINUTE;
+    this.seconds = Math.floor(remainingTime / TimeSpan.SECOND);
+
+    this.timerId = window.setInterval(() => this.tick(), TimeSpan.SECOND);
+  }
+
+  ngOnDestroy(): void {
+    window.clearInterval(this.timerId);
   }
 
   private tick(): void {
@@ -47,7 +69,10 @@ export class TimerComponent implements OnInit {
       this.seconds = 59;
       return;
     }
-
-    window.clearInterval(this.timerId);
+    
+    this.days = 13;
+    this.hours = 23;
+    this.minutes = 59;
+    this.seconds = 59;
   }
 }
